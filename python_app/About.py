73956821423b30f04964d4833f1843fd1d746cdc
mesmith454 <mesmith454@gmail.com>
2025-04-17 10:46:07 -0700

    added fortune values to test

--- python_app/About.py
+++ python_app/About.py
@@ -1,22 +1,35 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+import random
+
 app = Flask(__name__)
+
+fortunes = {
+    "colors": {
+        "red": ["RA", "RB"],
+        "yellow": ["YA", "YB"],
+        "blue": ["BA", "BB"],
+        "green": ["GA", "GB"],
+    },
+    "numbers": {
+        "1": ["1A", "1B"],
+        "2": ["2A", "2B"],
+        "3": ["3A", "3B"],
+        "4": ["4A", "4B"],
+    }
+}
 
 @app.route('/')
 def default():
-    return 'This is my first Flask App'
+    return render_template('fortune.html')
 
 @app.route('/about')
 def about():
     return 'Megan Mata, 33F, Network Administration Certification Track.'
 
-@app.route('/fortune',methods = ['GET', 'POST'])
-def fortune():
-    return render_template('fortune.html')
-        
-        # #retrieve the form data
-        # user = request.form['user']
-        # color = request.form['color']
-        # number = request.form['number']
+# @app.route('/get_fortune',methods = ['GET', 'POST'])
+
+    
+      
 
 
 if __name__ == '__main__':
