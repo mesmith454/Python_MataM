5a57ecdfaf59ba9a2d0a8e9c263157438ffa526a
mesmith454 <mesmith454@gmail.com>
2025-04-17 10:24:39 -0700

    began defining onclick functions

--- python_app/About.py
+++ python_app/About.py
@@ -10,20 +10,13 @@ def about():
     return 'Megan Mata, 33F, Network Administration Certification Track.'
 
 @app.route('/fortune',methods = ['GET', 'POST'])
-def fortune_form():
-    if request.method == 'POST':
-        return render_template('fortune.html')
+def fortune():
+    return render_template('fortune.html')
         
-    else:
-        #retrieve the form data
-        user = request.form['user']
-        color = request.form['color']
-        number = request.form['number']
-
-        return render_template('result.html', user=user, color=color, number=number)
-
-
-    
+        # #retrieve the form data
+        # user = request.form['user']
+        # color = request.form['color']
+        # number = request.form['number']
 
 
 if __name__ == '__main__':
