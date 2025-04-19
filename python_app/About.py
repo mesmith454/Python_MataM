bcb9b5c83f7d393240720f6e7699ad33252d7c9e
mesmith454 <mesmith454@gmail.com>
2025-04-19 13:46:06 -0700

    reset to basic app after much confusion. starting from scratch

--- python_app/About.py
+++ python_app/About.py
@@ -1,35 +1,19 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
 
-fortunes = {
-    "colors": {
-        "red": ["RA", "RB"],
-        "yellow": ["YA", "YB"],
-        "blue": ["BA", "BB"],
-        "green": ["GA", "GB"],
-    },
-    "numbers": {
-        "1": ["1A", "1B"],
-        "2": ["2A", "2B"],
-        "3": ["3A", "3B"],
-        "4": ["4A", "4B"],
-    }
-}
-
 @app.route('/')
 def default():
-    return render_template('fortune.html')
+    return 'Welcome to my first flask app'
 
 @app.route('/about')
 def about():
     return 'Megan Mata, 33F, Network Administration Certification Track.'
 
-# @app.route('/get_fortune',methods = ['GET', 'POST'])
-
-    
-      
+@app.route('/fortune',methods = ['GET', 'POST'])
+def get_fortune():
+    return render_template('fortune.html')
 
 
 if __name__ == '__main__':
