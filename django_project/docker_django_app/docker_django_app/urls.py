84451a0b61be5b9308a895c55facd069fa4efc64
mesmith454 <mesmith454@gmail.com>
2025-05-30 11:33:47 -0700

    simplifying url routing caused problems with db routing. the importance of mapping before building

--- django_project/docker_django_app/docker_django_app/urls.py
+++ django_project/docker_django_app/docker_django_app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('base_view/', include('base_view.urls')),
+    path('', include('base_view.urls')),
     path('admin/', admin.site.urls),
 ]
