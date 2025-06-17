4327b4402fd8c79cea64c141b7ddf8c3258a6a92
mesmith454 <mesmith454@gmail.com>
2025-06-17 13:21:52 -0700

    built api but unable to debug no module error: rest_framework not found

--- django_project/docker_django_app/base_view/urls.py
+++ django_project/docker_django_app/base_view/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
     path('', include('heroes.urls')),
     path('base_view/', views.base_view, name="base_view"),
     path('base_view/<str:name>/', views.greeting, name='greeting'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls), 
+    path('api/', include('api.urls')),
 ]
--- django_project/docker_django_app/docker_django_app/settings.py
+++ django_project/docker_django_app/docker_django_app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base_view',
-    'heroes'
+    'heroes',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
